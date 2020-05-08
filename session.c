dec033b16113aadef57c226d78d6fd52b6eb4cd6
Brooks Davis <brooks@one-eyed-alien.net>
2020-05-08 23:54:26 +0100

    Const things.

--- session.c
+++ session.c
@@ -1438,7 +1438,7 @@ do_nologin(struct passwd *pw)
 #ifdef HAVE_LOGIN_CAP
 	if (login_getcapbool(lc, "ignorenologin", 0) || pw->pw_uid == 0)
 		return;
-	nl = login_getcapstr(lc, "nologin", def_nl, def_nl);
+	nl = (char *)(uintptr_t)login_getcapstr(lc, "nologin", def_nl, def_nl);
 #else
 	if (pw->pw_uid == 0)
 		return;
